03061b3e43bbdf944d9975ec6fb8aebd38892c53
Harsh Dubey <86558899+Hrshhh@users.noreply.github.com>
2023-01-17 17:56:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,11 @@ import Shopping from './components/Shopping/Shopping';
 
 
 const App = () => {
-  // const [buying, setBuying] = useState(shop_items);
-
-  // const addShopHandler = buy => {
-  //   console.log("latrst",buy);
-  //   console.log("old",buying)
-  //   setBuying([...buying,...buy])
-  
-  // };
-
-  // const deleteHandler =(idd) => {
-  //   const removeShop = buying.filter((shop) => shop.id !== idd);
-  //   setBuying(removeShop);
-  // }
-
 
   return (
     <div>
       <NewShopping />
       <Shopping />
-      {/* <NewShopping onAddShop={addShopHandler} />
-      <Shopping items={buying} deleteShop={deleteHandler}/> */}
     </div>
   );
 }
