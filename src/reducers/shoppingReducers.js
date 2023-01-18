a62c92398d4a62743b0657a369c074aba876565a
Harsh Dubey <86558899+Hrshhh@users.noreply.github.com>
2023-01-18 19:09:00 +0530

    Update shoppingReducers.js

--- src/reducers/shoppingReducers.js
+++ src/reducers/shoppingReducers.js
@@ -1,35 +1,50 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const initialState= {
     shop_items: [
         {
             id: 'sd1',
-            title: 'Bhindi',
-            area: "Mumbai Suburban",
-            category: "Grocery",
-            opn_date: "2021-2-2",
-            cls_date: "2021-2-19"
+            text: {
+                title: 'Bhindi',
+                area: "Mumbai Suburban",
+                category: "Grocery",
+                opn_date: "2021-2-2",
+                cls_date: "2021-2-19"
+            }
+            
         },
-        { id: 'sd2',
-            title: 'Rounder',
-            area: "Thane",
-            category: "Stationery", 
-            opn_date: "2021-2-12",
-            cls_date: "2021-2-14"
+        { 
+            id: 'sd2',
+            text: {
+                title: 'Rounder',
+                area: "Thane",
+                category: "Stationery", 
+                opn_date: "2021-2-12",
+                cls_date: "2021-2-14"
+            }
+            
         },
         {
             id: 'sd3',
-            title: 'Chocolate Cake',
-            area: "Pune",
-            category: "Baker",
-            opn_date: "2022-12-09",
-            cls_date: "2022-12-09"
+            text: {
+                title: 'Chocolate Cake',
+                area: "Pune",
+                category: "Baker",
+                opn_date: "2022-12-09",
+                cls_date: "2022-12-09"
+            }
+            
         },
         {
             id: 'sd4',
-            title: 'Paracetamol',
-            area: "Nagpur",
-            category: "Chemist",
-            opn_date: "2022-12-09",
-            cls_date: "2022-12-10"
+            text: {
+                title: 'Paracetamol',
+                area: "Nagpur",
+                category: "Chemist",
+                opn_date: "2022-12-09",
+                cls_date: "2022-12-10"
+            }
+            
         },
     ]
 }
@@ -38,9 +53,13 @@ const initialState= {
 const shoppingReducers = (state=initialState, action) => {
     switch(action.type) {
         case ("ADD_TODO"):
+            const ShoppingWithId = {
+                id: uuidv4(),
+                text: action.payload.data,
+            };
             return{
                 ...state,
-                shop_items: [...state.shop_items, action.payload.data]
+                shop_items: [...state.shop_items, ShoppingWithId]
             }
 
         case ("DELETE_TODO"):
